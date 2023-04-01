cee7cf9316c5b62c0272211691cddc34c34a283c
JavierGarciaEstrada <baevaj@naver.com>
2023-03-31 20:51:23 -0600

    Elaboracion de la practica de clases hijas

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat extends Animal {
+
+
+    public Cat() {
+    }
+
+    public Cat(String name, int age, String color, String owner, String animelType, double weight, String breed, String sound) {
+        super(name, age, color, owner, animelType, weight, breed, sound);
+    }
+
+
+}
